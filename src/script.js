0a5e3bd6d2efdac8721b696d9af43b35cd97a94a
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-03-21 21:40:13 +0000

    Added functionality to the search engine(Api)

--- src/script.js
+++ src/script.js
@@ -1,8 +1,17 @@
+function currentTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let weather = document.querySelector("#temperatureData");
+  weather.innerHTML = Math.round(response.data.temperature.current);
+}
+
 function weatherApp(event) {
   event.preventDefault();
   let element = document.querySelector("#city-form");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = element.value;
+  let city = element.value;
+  let ApiKey = "8e4efa33a280aof6c33fe6a0t3ab54ec";
+  let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}&units=metric`;
+  axios.get(ApiUrl).then(currentTemperature);
 }
 
 let formElement = document.querySelector("#form-input");
