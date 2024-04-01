c6a49bb53dc64dfd3e24250215009a15958cc826
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-04-01 23:28:25 +0100

    updated

--- src/script.js
+++ src/script.js
@@ -1,8 +1,37 @@
 function currentTemperature(response) {
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
   let weather = document.querySelector("#temperatureData");
+  let conditionElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
+  console.log(response.data.time);
+  dateElement.innerHTML = weatherDate(date);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  conditionElement.innerHTML = response.data.condition.description;
+  windElement.innerHTML = `${response.data.wind.speed}Km/h`;
   weather.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+}
+function weatherDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10) hours = `0${hours}`;
+  if (minutes < 10) minutes = `0${minutes}`;
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function weatherApp(city) {
