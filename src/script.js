dc1a8845db6bac81c30a19fbfb3c17ba26390993
Ayokunnumi247 <163436878+Ayokunnumi247@users.noreply.github.com>
2024-04-01 16:58:10 +0100

    added functionality using Api

--- src/script.js
+++ src/script.js
@@ -5,14 +5,19 @@ function currentTemperature(response) {
   weather.innerHTML = Math.round(response.data.temperature.current);
 }
 
-function weatherApp(event) {
-  event.preventDefault();
-  let element = document.querySelector("#city-form");
-  let city = element.value;
+function weatherApp(city) {
   let ApiKey = "8e4efa33a280aof6c33fe6a0t3ab54ec";
   let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}&units=metric`;
   axios.get(ApiUrl).then(currentTemperature);
 }
 
+function cityTempo(event) {
+  event.preventDefault();
+  let element = document.querySelector("#city-form");
+  weatherApp(element.value);
+}
+
 let formElement = document.querySelector("#form-input");
-formElement.addEventListener("submit", weatherApp);
+formElement.addEventListener("submit", cityTempo);
+
+weatherApp("lisbon");
